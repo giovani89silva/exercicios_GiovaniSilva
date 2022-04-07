095a413beab81b0969e8e69d3dfea5fdf6cae662
giovani89silva <giovani.tiagosilva@yahoo.com.br>
2022-04-07 00:55:11 -0300

    segunda versão do codigo

--- src/main/java/Exercicio_Carro.java
+++ src/main/java/Exercicio_Carro.java
@@ -1,6 +1,6 @@
 public class Exercicio_Carro {
     public static void main(String[] args) {
-        System.out.println("teste para verificar se deu certo");
+        System.out.println("teste para verificar se deu certo dentro de outra versão");
 }
     
 }
